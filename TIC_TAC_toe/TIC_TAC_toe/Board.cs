db3ab6b7b4541fd32983c98ef9471291a6ad8ec7
Shubham <s.shrikhande1@gmail.com>
2022-02-10 12:11:37 +0530

    [Shubham]:Added this Board file

---
+++ TIC_TAC_toe/TIC_TAC_toe/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIC_TAC_toe
+{
+    public class Board
+    {
+
+    
+        char[] board = new char[10];
+        public void gameBoard()
+        {
+
+            for (int i = 1; i < 10; i++)
+            {
+                board[i] = ' ';
+            }
+        }
+    }
+}
